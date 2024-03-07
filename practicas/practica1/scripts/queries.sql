8f991728a13364993b3b4fc8fa92c65484fef29c
ElJoamy <josephsalguero1@upb.edu>
2024-03-06 22:22:52 -0400

    Agregadas consultas de INNER JOIN, LEFT JOIN, RIGHT JOIN, FULL JOIN, CROSS JOIN y SELF JOIN en queries.sql

--- practicas/practica1/scripts/queries.sql
+++ practicas/practica1/scripts/queries.sql
@@ -1,1 +1,34 @@
--- Nombre: XXXXXXXXXXXXXXXXXXX
+-- Nombre y Apellido: XXXXXXXXXXXXXXXXXXX
+
+-- INNERS JOIN
+-- Ejercicio 1
+
+
+-- Ejercicio 2
+
+
+-- LEFT JOIN
+-- Ejercicio 1
+
+
+-- Ejercicio 2
+
+
+-- RIGHT JOIN
+-- Ejercicio 1
+
+
+-- Ejercicio 2
+
+
+-- FULL JOIN
+-- Ejercicio
+
+
+-- CROSS JOIN
+-- Ejercicio
+
+
+-- SELF JOIN
+-- Ejercicio
+
