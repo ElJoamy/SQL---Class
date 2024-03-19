f63c04c2679fe39659f87052028d8295abc23a87
ElJoamy <josephsalguero1@upb.edu>
2024-03-19 19:10:24 -0400

    Agregadas consultas y vistas en el archivo queries.sql

---
+++ practicas/practica2/scripts/queries.sql
@@ -0,0 +1,36 @@
+-- Nombre y Apellido: XXXXXXXXXXXXXXXXXXX
+
+-- Group By
+-- 1: Cantidad de proyecciones por película:
+-- Respuesta
+
+
+-- 2: Ingresos generados por película, ordenados de mayor a menor:
+-- Respuesta
+
+
+-- Having
+-- 1: Películas con un precio promedio de entrada mayor a 50:
+-- Respuesta
+
+
+-- 2: Salas que han mostrado un amplio rango de géneros de películas:
+-- Respuesta
+
+
+-- Subconsultas
+-- 1: Películas que no han sido proyectadas:
+-- Respuesta
+
+
+-- 2: Clientes que han asistido a todas las películas de un cierto género:
+-- Respuesta
+
+
+-- Vistas
+-- 1: Vista de detalles de proyecciones:
+-- Respuesta
+
+
+-- 2: Vista de ingresos y popularidad por género de película:
+-- Respuesta
